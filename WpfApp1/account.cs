2a2881bba4ecc090d8c6cd572b40ef3dca7677b0
JustVlad01 <s00233992@atu.ie>
2024-01-05 18:38:13 +0000

    Add project files.

---
+++ WpfApp1/account.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+public abstract class Account
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public decimal Balance { get; protected set; }
+    public DateTime InterestDate { get; protected set; }
+
+    public Account(string firstName, string lastName, decimal balance)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Balance = balance;
+        InterestDate = DateTime.Now;
+    }
+
+    public void Deposit(decimal amount)
+    {
+        if (amount > 0)
+        {
+            Balance += amount;
+        }
+    }
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount > 0 && amount <= Balance)
+        {
+            Balance -= amount;
+        }
+    }
+
+    public abstract void CalculateInterest();
+}
+
+public class CurrentAccount : Account
+{
+    public decimal InterestRate { get; }
+
+    public CurrentAccount(string firstName, string lastName, decimal balance)
+        : base(firstName, lastName, balance)
+    {
+        InterestRate = 0.03m; // 3% interest rate for current account
+    }
+
+    public override void CalculateInterest()
+    {
+        // Implement the calculation logic for current account interest here
+        // For example:
+        decimal interestAmount = Balance * InterestRate;
+        Balance += interestAmount;
+        InterestDate = DateTime.Now;
+    }
+}
+
+public class SavingsAccount : Account
+{
+    public decimal InterestRate { get; }
+
+    public SavingsAccount(string firstName, string lastName, decimal balance)
+        : base(firstName, lastName, balance)
+    {
+        InterestRate = 0.06m; // 6% interest rate for savings account
+    }
+
+    public override void CalculateInterest()
+    {
+        // Implement the calculation logic for savings account interest here
+        // For example:
+        decimal interestAmount = Balance * InterestRate;
+        Balance += interestAmount;
+        InterestDate = DateTime.Now;
+    }
+}
